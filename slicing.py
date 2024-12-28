cfa9bbb34edb25f08b31e50ccad16086119dcd49
phyomin856 <phyominkhant106@gmail.com>
2024-12-28 11:20:56 +0800

    slicing and indexing

---
+++ slicing.py
@@ -0,0 +1,17 @@
+name = "phyominkhant"
+##### Indexing #####
+# print(name[0])
+# print(name[1])
+# print(name[4])
+
+##### Slicing #####
+# print(name[0:4:1])
+# print(name[0:7:2])
+
+##### Original copy of name #####
+copyName = name[:]
+print(copyName)
+
+##### Reverse copy of name #####
+copy_Name = name[::-1]
+print(copy_Name)
