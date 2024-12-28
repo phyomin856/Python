a64a4548f2badb19f97dc105eedae53b1f3fe6cb
phyomin856 <phyominkhant106@gmail.com>
2024-12-28 16:06:34 +0800

    stringmethod

---
+++ stringmethod.py
@@ -0,0 +1,12 @@
+name = "phyo min min khant"
+# result = name.isalpha()
+# print(f"Result : {result}")
+
+# result = name.capitalize()
+# print(f"Result : {result}")
+
+# result = name.replace("k","t")
+# print(f"Result : {result}")
+
+result = name.index("min")
+print(f"Result : {result}")
