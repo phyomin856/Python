7a2e1e03487305e9818bb0c793f62428987cbc78
phyomin856 <phyominkhant106@gmail.com>
2024-12-28 11:41:22 +0800

    usingprintfun

---
+++ usingprintfun.py
@@ -0,0 +1,11 @@
+##### Using print function by using format method and format string #####
+
+name = "Phyo Min Khant"
+job = "Student"
+
+##### Format Method #####
+print("My name is {} and I am a {}".format(name,job))
+print("My name is {1} and I am a {0}".format(name,job))
+
+##### Format String #####
+print(f"My name is {name} and I am a {job}.")
