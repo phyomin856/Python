6fced0da5d5f871cc82bf89cff10b1ce3418810a
phyomin856 <phyominkhant106@gmail.com>
2024-12-28 11:44:17 +0800

    usingprintfun sec

--- usingprintfun.py
+++ usingprintfun.py
@@ -9,3 +9,9 @@ print("My name is {1} and I am a {0}".format(name,job))
 
 ##### Format String #####
 print(f"My name is {name} and I am a {job}.")
+
+Name = "Phyo Min Khant"
+Job = "Student"
+age = 22
+print(f"My name is {Name} and I am a {Job} and {age} years old.")
+print("My name is {} and I am a {} and {} years old.".format(Name,Job,age))
