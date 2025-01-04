23a76dbb3a904d02a8b9509fea9100a0b0368957
phyomin856 <phyominkhant106@gmail.com>
2025-01-04 14:44:55 +0800

    Indexing and Slicing

---
+++ hw.py
@@ -0,0 +1,13 @@
+list = [78,92,85,88,95,89]
+print(list[2])
+list[1] = 96
+secondStudentGrade = list[1]
+print(list)
+newStudentGrade = list.append(90)
+print(list)
+print(list[:3])
+high_grades = [96,95,90]
+print(f"Highest grades are {high_grades}")
+print(list[::-1])
+every_other = list[1::]
+print(every_other)
